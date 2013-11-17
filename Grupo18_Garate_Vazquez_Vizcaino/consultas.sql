07d173a00aad847b5955461a4ddf678881850b3c
krahser <aldo.vizcaino87@gmail.com>
2013-11-17 16:28:29 -0300

    1 y 2 sql

--- Grupo18_Garate_Vazquez_Vizcaino/consultas.sql
+++ Grupo18_Garate_Vazquez_Vizcaino/consultas.sql
@@ -1,44 +1,21 @@
-#.
+1.
 **Consulta normalizada**
-
 ```
-select dniPaciente, nombreApellidoPaciente from paciente order by
-dniPaciente asc
+select dniPaciente, nombreApellidoPaciente from paciente order by dniPaciente asc
 ```
 **Consulta desnormalizada**
 
 ``` 
-select distinct(dniPaciente), nombreApellidoPaciente from internacion order by
-dniPaciente asc
+select distinct(dniPaciente), nombreApellidoPaciente from internacion order by asc
 ```
 
-#.
-
-Normalizada
-===========
-
-
-Con el join tarda mas en obtener los resultados
+2.
+**consulta normalizada**
 ```
-/*3 que pasa si la hago con un left join y borro una subconsulta*/
-select dniPaciente, nombreApellidoPaciente from paciente p where  not
-exists (select * from internacion i where p.dniPaciente =
-i.dniPaciente and obraSocialInternacion = obraSecundaria) and exists
-(select * from internacion i where p.dniPaciente = i.dniPaciente and
-obraSocialInternacion = obraPrimaria);
+ select dniPaciente, nombreApellidoPaciente from paciente p where not exists (select * from internacion i where obraSecundaria = obraSocialInternacion and p.dniPaciente=i.dniPaciente);
 ```
-Con el distinct tarda mas la duracion de la consulta
+**consulta desnormalizada**
 ```
-select distinct(p.dniPaciente), p.nombreApellidoPaciente from paciente p right outer join internacion i on (i.dniPaciente=p.dniPaciente) where  not exists (select * from internacion i where p.dniPaciente = i.dniPaciente and obraSocialInternacion = obraSecundaria) and obraSocialInternacion = obraPrimaria;
-```
-
-desnormalizada
-==============
-
-La segunda consulta es mas rapida
-```
-/*3*/
 select distinct(dniPaciente), nombreApellidoPaciente from internacion p where  not exists (select * from internacion i where p.dniPaciente = i.dniPaciente and i.obraSocialInternacion = p.obraSecundaria) and exists (select * from internacion i where p.dniPaciente = i.dniPaciente and i.obraSocialInternacion = p.obraPrimaria);
-select distinct(dniPaciente), nombreApellidoPaciente from internacion where dniPaciente not in (select dniPaciente from internacion where obraSecundaria = obraSocialInternacion)
 ```
-
+3.
